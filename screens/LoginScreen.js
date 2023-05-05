0754f87a7d7ec39773592c2bdedebf00f153944b
anas <anas2002saadi@gmail.com>
2023-05-05 23:51:59 +0000

    Login and Home

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput } from 'react-native'
 import React from 'react'
 
 export default function LoginScreen() {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior='padding'>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Email" 
+         //value={}
+         //onChangeText={text =>}
+         style={styles.input}
+        />
+        <TextInput 
+         placeholder="Password" 
+         //value={}
+         //onChangeText={text =>}
+         style={styles.input}
+         SecureTextEntry
+        />
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
