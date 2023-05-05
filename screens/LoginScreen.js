422e9b893a57670870f6fb0768f55f7f43b89155
Rachdi <rachdimouhssine@gmail.com>
2023-05-06 01:57:44 +0200

    mouhssin's changes

---
+++ screens/LoginScreen.js
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput } from 'react-native'
+import React from 'react'
+
+export default function LoginScreen() {
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior='padding'>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Email" 
+         //value={}
+         //onChangeText={text =>}
+         style={styles.input}
+        />
+        <TextInput 
+         placeholder="Password" 
+         //value={}
+         //onChangeText={text =>}
+         style={styles.input}
+         SecureTextEntry
+        />
+      </View>
+    </KeyboardAvoidingView>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
