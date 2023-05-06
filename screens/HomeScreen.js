6b2588e0ea9ac38b4154d5dfd439d17c2a3e1df7
Rachdi <rachdimouhssine@gmail.com>
2023-05-06 02:01:01 +0200

    mouhssine's changes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 export default function HomeScreen() {
   return (
     <View>
       <Text>Home Screen</Text>
     </View>
-  )
+  );
 }
+/*hell*/
 
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,31 +1,39 @@
-import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+} from "react-native";
+import React from "react";
 
 export default function LoginScreen() {
+  /*hello*/
   return (
-    <KeyboardAvoidingView style={styles.container} behavior='padding'>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Email" 
-         //value={}
-         //onChangeText={text =>}
-         style={styles.input}
+        <TextInput
+          placeholder="Email"
+          //value={}
+          //onChangeText={text =>}
+          style={styles.input}
         />
-        <TextInput 
-         placeholder="Password" 
-         //value={}
-         //onChangeText={text =>}
-         style={styles.input}
-         SecureTextEntry
+        <TextInput
+          placeholder="Password"
+          //value={}
+          //onChangeText={text =>}
+          style={styles.input}
+          SecureTextEntry
         />
       </View>
     </KeyboardAvoidingView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
